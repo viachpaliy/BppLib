b5b224fc177b14d292930020f88fead4a79d611a
viachpaliy <viachpaliy@ukr.net>
2021-08-25 17:35:40 +0300

    add project for CID files

---
+++ BppLib.CIDFile/FileId.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace BppLib.CIDFile
+{
+    ///<summary>Class <c>FileId</c> models ID section in Biesse CID program.</summary>
+    public class FileId
+    {
+        ///<value> Property <c>FileName</c> represents the name of the Biesse CID program.</value>
+        public string FileName { get; set; } = "Panel";
+
+        ///<value>Property <c>Rel</c> represents the release of CID programme.</value>
+        public string Rel { get; set; } = "3.0";
+
+        ///<value>Property <c>Axis</c> represents the machine axes direction.</value>
+        public string Axis { get; set; } = "x+,y-,z-";
+
+        /// <summary>This method serializes an object as Cid code.</summary>
+		/// <returns>A string  is coded as Cid code.</returns>
+        public string AsCidCode()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("BEGIN ID CID3");
+            sb.AppendLine("'\"" + FileName + "\"");
+            sb.AppendLine("  REL= " + Rel);
+            sb.AppendLine("  AXIS=" + Axis);
+            sb.Append("END ID");
+            return sb.ToString();
+        }
+    }
+}
---
+++ BppLib.UnitTests/BppLib.CIDFile.UnitTests/FileIdTest.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using BppLib.CIDFile;
+
+namespace BppLib.CIDFile.UnitTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        
+
+        [Test]
+        public void AsCidCodeTest()
+        {
+            var obj = new FileId();
+            obj.FileName = "SimpleBoring";
+            string expected = @"BEGIN ID CID3
+'""SimpleBoring""
+  REL= 3.0
+  AXIS=x+,y-,z-
+END ID";
+            Assert.AreEqual(expected, obj.AsCidCode());
+        }
+    }
+}
